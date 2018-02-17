85e2f729a4bcfbf28abfe77fc10306e1ffa43d11
Jean Pierre Charalambos <nakednous@gmail.com>
2018-02-17 11:52:45 -0500

    configs updated

--- src/frames/processing/Scene.java
+++ src/frames/processing/Scene.java
@@ -189,8 +189,8 @@ import java.util.List;
 public class Scene extends Graph implements PConstants {
   // Timing
   protected boolean _javaTiming;
-  public static String prettyVersion = "3.0.0";
-  public static String version = "32";
+  public static String prettyVersion = "0.1.0";
+  public static String version = "1";
 
   // P R O C E S S I N G A P P L E T A N D O B J E C T S
   protected PApplet _parent;
