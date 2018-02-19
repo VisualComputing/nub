37c68ba952c112f08ed42c5add56145f85e1eccb
Sebastian Chaparro <sechaparroc@unal.edu.co>
2018-02-18 19:59:20 -0500

    Functionality to use Standard rotation in IK has been added

--- src/frames/core/Node.java
+++ src/frames/core/Node.java
@@ -2807,4 +2807,78 @@ public class Node extends Frame implements Grabber {
     }
     return false;
   }
+
+
+  //SOME USEFUL METHODS FOR IK
+
+  /**Set the orientation of this Node when it is not pointing to its child**/
+  private void fixRotation(Node child){
+    fixRotation(child.translation());
+  }
+
+  /**
+   * Set orientation of a Node in the following way:
+   * When Graph is 3D:
+   *  1. Z-Axis points to direction vector
+   *  2. Y-Axis will be orthogonal to Z-Axis Parent Node and Z-Axis.
+   *  3. Y-Axis Parent Node and Y-Axis must have same Sign
+   * When Graph is 2D:
+   *  1. X-Axis points to direction vector
+   * Returns the Quaternion that allows the described Transformation
+   * **/
+  private Quaternion fixRotation(Vector direction){
+    Vector x = new Vector(1,0,0);
+    Vector y = new Vector(0,1,0);
+    Vector z = new Vector(0,0,1);
+    Quaternion rotation = rotation().inverse();
+    Quaternion inverse_rotation;
+    rotate(rotation);
+    if(this.graph().is3D()){
+      /*First we get the rotation required to set Z-Axis in the direction of the child new position*/
+      Quaternion z_rotation = new Quaternion(z, rotation.inverse().rotate(direction));
+      rotate(z_rotation);
+      /*Then we let Y-Axis to be orthogonal to Z_parent and Z*/
+      Vector z_parent = localTransformOf(z);
+      Vector y_direction = new Vector();
+      Vector.cross(z_parent,z, y_direction);
+      //Check dot product
+      if(z_parent.dot(z) < 0){
+        y_direction.multiply(-1);
+      }
+      /*Get how much Y-Axis must rotate*/
+      if(y_direction.magnitude() > 1e-6f) {
+        Quaternion y_rotation = new Quaternion(y, y_direction);
+        rotate(y_rotation);
+        rotation.compose(Quaternion.compose(z_rotation, y_rotation));
+        inverse_rotation = rotation.inverse();
+      }else{
+        rotation.compose(z_rotation);
+        inverse_rotation = rotation.inverse();
+      }
+    }else{
+      /*First we get the rotation required to set X-Axis in the direction of the child new position*/
+      rotation.compose(new Quaternion(x, direction));
+      inverse_rotation = rotation.inverse();
+      rotate(rotation);
+    }
+    //Apply inverse rotation to each children
+    for(int i = 0; i < children().size(); i++) {
+      children().get(i).setRotation(Quaternion.compose(inverse_rotation, children().get(i).rotation()));
+      children().get(i).setTranslation(new Vector(0, 0, children().get(i).translation().magnitude()));
+    }
+    return rotation;
+  }
+
+  /**
+   * From the branch specified by this as root, change Nodes rotation to follow  Standard Notation
+   */
+  public void setupHierarchy(){
+    if(children().isEmpty()) setRotation(new Quaternion());
+    if(children().size() == 1) fixRotation(children().get(0));
+    for(Node node : children()) {
+      node.setupHierarchy();
+    }
+  }
+
+
 }
