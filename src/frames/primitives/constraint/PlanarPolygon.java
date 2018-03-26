e7f424597245942941b73b43ffd0336f570a4846
Sebastian Chaparro <sechaparroc@unal.edu.co>
2018-03-25 23:57:43 -0500

    Constraints could be used with arbitrary axis

--- src/frames/primitives/constraint/PlanarPolygon.java
+++ src/frames/primitives/constraint/PlanarPolygon.java
@@ -40,6 +40,20 @@ public class PlanarPolygon extends Constraint {
     this._restRotation = restRotation.get();
   }
 
+  /**
+   * reference is a Quaternion that will be aligned to point to the given Basis Vectors
+   * result will be stored on restRotation.
+   * twist and up axis are defined locally on reference rotation
+   * */
+  public void setRestRotation(Quaternion reference, Vector up, Vector twist){
+    _restRotation = reference.get();
+    Vector Z = _restRotation.inverse().rotate(twist);
+    //Align Y-Axis with Up Axis
+    _restRotation.compose(new Quaternion(new Vector(0,1,0), up));
+    //Align y-Axis with twist vector
+    _restRotation.compose(new Quaternion(new Vector(0,0,1), twist));
+  }
+
   public ArrayList<Vector> vertices() {
     return _vertices;
   }
@@ -91,15 +105,17 @@ public class PlanarPolygon extends Constraint {
 
   @Override
   public Quaternion constrainRotation(Quaternion rotation, Frame frame) {
-        /*
-            if(frame.is2D())
-            throw new RuntimeException("This constrained not supports 2D Frames");
-        */
+    /*
+        if(frame.is2D())
+        throw new RuntimeException("This constrained not supports 2D Frames");
+    */
     Quaternion desired = Quaternion.compose(frame.rotation(), rotation);
-    Vector new_pos = Quaternion.multiply(desired, new Vector(0, 0, 1));
+    //twist to frame
+    Vector twist = _restRotation.rotate(new Vector(0,0,1));
+    Vector new_pos = Quaternion.multiply(desired, twist);
     Vector constrained = apply(new_pos, _restRotation);
     //Get Quaternion
-    return new Quaternion(new Vector(0, 0, 1), Quaternion.multiply(frame.rotation().inverse(), constrained));
+    return new Quaternion(twist, Quaternion.multiply(frame.rotation().inverse(), constrained));
   }
 
 
