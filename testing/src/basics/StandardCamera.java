d1794afcb0a639389d140cb63d3a7b1bed53a4c5
Jean Pierre Charalambos <nakednous@gmail.com>
2018-06-01 10:09:15 -0500

    StandardCamera example added to testing

---
+++ testing/src/basics/StandardCamera.java
@@ -0,0 +1,128 @@
+package basics;
+
+import frames.core.Frame;
+import frames.primitives.Vector;
+import frames.processing.Scene;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+
+public class StandardCamera extends PApplet {
+  Scene scene, auxScene;
+  PGraphics canvas, auxCanvas;
+  StdCamera stdCam;
+  Frame origCam;
+
+  int w = 1110;
+  int h = 1110;
+
+  public void settings() {
+    size(w, h, P3D);
+  }
+
+  public void setup() {
+    canvas = createGraphics(w, h / 2, P3D);
+    scene = new StdCamera(this, canvas);
+    scene.setRadius(200);
+    scene.fitBallInterpolation();
+
+    // enable computation of the frustum planes equations (disabled by default)
+    scene.enableBoundaryEquations();
+
+    auxCanvas = createGraphics(w, h / 2, P3D);
+    // Note that we pass the upper left corner coordinates where the scene
+    // is to be drawn (see drawing code below) to its constructor.
+    auxScene = new Scene(this, auxCanvas, 0, h / 2);
+    auxScene.setRadius(400);
+    auxScene.fitBallInterpolation();
+  }
+
+  void mainDrawing(Scene s) {
+    PGraphics p = s.frontBuffer();
+    p.background(0);
+    p.noStroke();
+    // the main viewer camera is used to cull the sphere object against its frustum
+    switch (scene.ballVisibility(new Vector(0, 0, 0), scene.radius() * 0.6f)) {
+      case VISIBLE:
+        p.fill(0, 255, 0);
+        p.sphere(scene.radius() * 0.6f);
+        break;
+      case SEMIVISIBLE:
+        p.fill(255, 0, 0);
+        p.sphere(scene.radius() * 0.6f);
+        break;
+      case INVISIBLE:
+        break;
+    }
+  }
+
+  void auxiliarDrawing(Scene s) {
+    mainDrawing(s);
+    PGraphics p = s.frontBuffer();
+    p.pushStyle();
+    p.stroke(255, 255, 0);
+    p.fill(255, 255, 0, 160);
+    s.drawEye(scene);
+    p.popStyle();
+  }
+
+  public void draw() {
+    scene.beginDraw();
+    mainDrawing(scene);
+    scene.endDraw();
+    scene.display();
+
+    auxScene.beginDraw();
+    auxiliarDrawing(auxScene);
+    auxScene.endDraw();
+    auxScene.display();
+  }
+
+  public static void main(String args[]) {
+    PApplet.main(new String[]{"basics.StandardCamera"});
+  }
+
+  public class StdCamera extends Scene {
+    boolean standard;
+
+    public StdCamera(PApplet applet) {
+      super(applet);
+      standard = false;
+    }
+
+    public StdCamera(PApplet applet, PGraphics pg) {
+      super(applet, pg);
+      standard = false;
+    }
+
+    public void toggleMode() {
+      standard = !standard;
+    }
+
+    public boolean isStandard() {
+      return standard;
+    }
+
+    @Override
+    public float zNear() {
+      if (standard)
+        return 0.001f;
+      else
+        return super.zNear();
+    }
+
+    @Override
+    public float zFar() {
+      if (standard)
+        return 1000.0f;
+      else
+        return super.zFar();
+    }
+
+    @Override
+    protected float _rescalingFactor() {
+      if (isStandard())
+        return 1.0f;
+      return super._rescalingFactor();
+    }
+  }
+}
