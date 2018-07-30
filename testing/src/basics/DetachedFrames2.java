163b074922171b5a458837921975128263b38570
Jean Pierre Charalambos <nakednous@gmail.com>
2018-07-30 08:05:41 -0500

    Frame random stuff overloaded

--- testing/src/basics/DetachedFrames2.java
+++ testing/src/basics/DetachedFrames2.java
@@ -1,9 +1,11 @@
 package basics;
 
 import frames.core.Frame;
+import frames.primitives.Matrix;
 import frames.primitives.Vector;
 import frames.processing.Scene;
 import processing.core.PApplet;
+import processing.opengl.PGraphicsOpenGL;
 
 /**
  * Created by pierre on 11/15/16.
@@ -19,10 +21,10 @@ public class DetachedFrames2 extends PApplet {
   public void setup() {
     eye = new Frame();
     eye.setPosition(0, 0, 200);
-    frames = new Frame[50];
+    frames = new Frame[5];
     for (int i = 0; i < frames.length; i++) {
       frames[i] = new Frame();
-      frames[i].randomize(new Vector(), 100);
+      frames[i].randomize();
     }
   }
 
@@ -31,6 +33,7 @@ public class DetachedFrames2 extends PApplet {
     float fov = PI / 3.0f;
     float cameraZ = (height / 2.0f) / tan(fov / 2.0f);
     perspective(fov, width / height, cameraZ / 10.0f, cameraZ * 10.0f);
+    //((PGraphicsOpenGL)g).setProjection(Scene.toPMatrix(Matrix.perspective(cameraZ / 10.0f, cameraZ * 10.0f, width / height, -tan(fov / 2.0f))));
     //resetMatrix();
     //applyMatrix(Scene.toPMatrix(eye.view()));
     setMatrix(Scene.toPMatrix(eye.view()));
