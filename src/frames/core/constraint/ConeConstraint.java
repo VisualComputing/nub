bfe245b45fa1f727f6af0720429df2f426ac7b0e
sechaparroc <sebchaparr@gmail.com>
2019-03-16 23:43:54 -0500

    Cone constraint with swing/twist decomposition

--- src/frames/core/constraint/ConeConstraint.java
+++ src/frames/core/constraint/ConeConstraint.java
@@ -53,21 +53,15 @@ public abstract class ConeConstraint extends Constraint{
     public void setRestRotation(Quaternion reference, Vector up, Vector twist, Vector rest) {
         _orientation = reference.get();
         _idleRotation = reference.get();
+        //Align y-Axis with up vector
         Quaternion delta = new Quaternion(new Vector(0, 1, 0), up);
         Vector tw = delta.inverseRotate(twist);
+        //Align z-Axis with twist vector
         delta.compose(new Quaternion(new Vector(0, 0, 1), tw));
-        _orientation.compose(delta);
+        _orientation.compose(delta); // orientation = idle * rest
         Vector rs = delta.inverseRotate(rest);
-        off = new Quaternion(twist, rest);
-        //_idleRotation = Quaternion.compose(_idleRotation, off.inverse());
-        //delta.compose(new Quaternion(new Vector(0, 0, 1), rs));//Quaternion.compose(_idleRotation.inverse(), q);
+        off = new Quaternion(twist, rest); // TODO : check offset
         _restRotation = delta;
-        //_orientation.compose(delta);
-        //Align y-Axis with twist vector
-        //_orientation.compose(new Quaternion(new Vector(0, 0, 1), tw));
-        //Vector rs = new Quaternion(new Vector(0, 1, 0), up).inverseRotate(rest);
-        //Quaternion q = Quaternion.compose(_orientation,new Quaternion(rs, new Vector(0, 0, 1)));
-        //_restRotation = Quaternion.compose(_idleRotation.inverse(), q);
     }
 
 
@@ -77,11 +71,12 @@ public abstract class ConeConstraint extends Constraint{
 
     @Override
     public Quaternion constrainRotation(Quaternion rotation, Frame frame) {
+        //Define how much idle rotation must change : idle * change = frame * rot
         //Identify rotation that must be applied to move twist and up to its  new values
         Quaternion curr = _idleRotation; //w.r.t ref
         Quaternion next = Quaternion.compose(frame.rotation(), rotation); // w.r.t ref
         Quaternion change = Quaternion.compose(curr.inverse(), next); // w.r.t idle
-        //Decompose change in terms of twist and swing
+        //Decompose change in terms of twist and swing (twist vector w.r.t idle)
         Vector tw = _restRotation.rotate(new Vector(0, 0, 1)); // w.r.t idle
         Vector rotationAxis = new Vector(change._quaternion[0], change._quaternion[1], change._quaternion[2]);
         rotationAxis = Vector.projectVectorOnAxis(rotationAxis, tw); // w.r.t idle
@@ -89,17 +84,16 @@ public abstract class ConeConstraint extends Constraint{
         Quaternion rotationTwist = new Quaternion(rotationAxis.x(), rotationAxis.y(), rotationAxis.z(), change.w()); //w.r.t idle
         Quaternion rotationSwing = Quaternion.compose(change, rotationTwist.inverse()); //w.r.t idle
         //Constraint swing
-        Vector w = rotationSwing.rotate(_restRotation.rotate(new Vector(0, 0, 1)));
-        w = off.rotate(w);
-        Vector constrained = apply(w, _restRotation);
-        constrained = off.inverseRotate(constrained);
-        rotationSwing = new Quaternion(tw, constrained);
+        Vector new_pos = rotationSwing.rotate(_restRotation.rotate(new Vector(0, 0, 1))); //get twist desired target position
+        new_pos = off.rotate(new_pos); //apply offset if required
+        Vector constrained = apply(new_pos, _restRotation); // constraint target position
+        constrained = off.inverseRotate(constrained);// undo offset
+        rotationSwing = new Quaternion(tw, constrained); // get constrained swing rotation
         //constrained change
         Quaternion constrained_change = Quaternion.compose(rotationSwing, rotationTwist);
         //find change in terms of frame rot
         //_idle * constrained_change = frame * rot
         Quaternion rot = Quaternion.compose(frame.rotation().inverse(), Quaternion.compose(_idleRotation, constrained_change));
-
         return rot;
     }
 
