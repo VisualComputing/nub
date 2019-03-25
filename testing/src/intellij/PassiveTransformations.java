1a7d08797c67b0f51a7250a4409e7ce92f84ef73
Jean Pierre Charalambos <nakednous@gmail.com>
2019-03-25 16:02:20 -0500

    passive transformations added to testing

---
+++ testing/src/intellij/PassiveTransformations.java
@@ -0,0 +1,87 @@
+package intellij;
+
+import nub.primitives.*;
+import nub.core.*;
+import processing.core.*;
+import processing.event.*;
+import processing.opengl.*;
+
+public class PassiveTransformations extends PApplet {
+  Graph graph;
+  Node[] nodes;
+
+  public void settings() {
+    size(800, 800, P3D);
+  }
+
+  public void setup() {
+    graph = new Graph(width, height);
+    GLSLMatrixHandler glslMatrixHandler = new GLSLMatrixHandler(graph);
+    graph.setMatrixHandler(glslMatrixHandler);
+    graph.setFOV(PI / 3);
+    graph.fit(1);
+    nodes = new Node[50];
+    for (int i = 0; i < nodes.length; i++) {
+      nodes[i] = new Node(graph) {
+        @Override
+        public void visit() {
+          pushStyle();
+          fill(isTracked(graph) ? 0 : 255, 0, 255);
+          box(5);
+          popStyle();
+        }
+      };
+      nodes[i].randomize();
+      nodes[i].setPickingThreshold(20);
+    }
+    //discard Processing matrices
+    resetMatrix();
+  }
+
+  public void draw() {
+    background(0);
+    graph.preDraw();
+    graph.render();
+  }
+
+  public void mouseMoved() {
+    graph.track(mouseX, mouseY, nodes);
+  }
+
+  public void mouseDragged() {
+    if (mouseButton == LEFT)
+      graph.spin(new Point(pmouseX, pmouseY), new Point(mouseX, mouseY));
+    else if (mouseButton == RIGHT)
+      graph.translate(mouseX - pmouseX, mouseY - pmouseY);
+    else
+      graph.scale(mouseX - pmouseX);
+  }
+
+  public void mouseWheel(MouseEvent event) {
+    graph.scale(event.getCount() * 20);
+  }
+
+  public class GLSLMatrixHandler extends MatrixHandler {
+    PShader _shader;
+    PMatrix3D _pmatrix = new PMatrix3D();
+
+    public GLSLMatrixHandler(Graph graph) {
+      super(graph.width(), graph.height());
+      _shader = loadShader("/home/pierre/IdeaProjects/nubjs/testing/data/matrix_handler/FrameFrag.glsl", "/home/pierre/IdeaProjects/nubjs/testing/data/matrix_handler/FrameVert_pmv.glsl");
+    }
+
+    @Override
+    protected void _setUniforms() {
+      shader(_shader);
+      // same as:
+      //pmatrix.set(Scene.toPMatrix(projectionModelView()));
+      //pmatrix.transpose();
+      _pmatrix.set(projectionModelView().get(new float[16]));
+      _shader.set("frames_transform", _pmatrix);
+    }
+  }
+
+  public static void main(String args[]) {
+    PApplet.main(new String[]{"intellij.PassiveTransformations"});
+  }
+}
