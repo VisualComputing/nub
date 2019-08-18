d391e7153055f042d142155629ceb22edf191360
Sebastian Chaparro <sebchaparr@gmail.com>
2019-08-18 18:46:45 -0500

    New solver heuristics updated

--- src/nub/ik/solver/geometric/MySolver.java
+++ src/nub/ik/solver/geometric/MySolver.java
@@ -18,6 +18,10 @@ public class MySolver extends Solver{
     protected Node _target;
     protected Node _prevTarget;
 
+    public MySolver(List<? extends Node> chain) {
+        this(chain, null);
+    }
+
     public MySolver(List<? extends Node> chain, Node target){
         super();
         this._original = chain;
@@ -26,38 +30,92 @@ public class MySolver extends Solver{
         _generateGlobalConstraints();
     }
 
-
+    int st = 0, cur = 0;
     @Override
     protected boolean _iterate() {
-        if(_target == null) return true;
+        //As no target is specified there is no need to perform an iteration
+        if (_target == null || _chain.size() < 2) return true;
+        Node end = _chain.get(_chain.size() - 1);
+        Vector target = this._target.position().get();
+        //Execute Until the distance between the end effector and the target is below a threshold
+        if (Vector.distance(end.position(), target) <= _maxError) {
+            return true;
+        }
+        //st = 0;
+        //while(st < _original.size() - 1) {
+            if(st <= _original.size() - 2){
+                _applyStep(_original, _reversed, st);
 
-
+            }
+            else{
+                _update(_original, _reversed);
+                _updateReverse(_original, _reversed);
+                st = 0;
+            }
+            //st++;
+        //}
+        //st = (st + 1) % (_original.size() - 1);
+        //Check total rotation change
         return false;
     }
 
+    protected void _update(List<? extends Node> chain, List<? extends Node> reversed){
+        for(int i = 0; i < chain.size()-1; i++){
+            Quaternion q = new Quaternion(chain.get(i).location(chain.get(i+1)), chain.get(i).location(reversed.get(reversed.size() - 2 - i)));
+           chain.get(i).rotate(q);
+        }
+    }
+
+    protected void _updateReverse(List<? extends Node> chain, List<? extends Node> reversed){
+        int n = reversed.size();
+        for(int i = 0; i < n-1; i++){
+            Vector v1 = Vector.subtract(reversed.get(i+1).position(), reversed.get(i).position());
+            Vector v2 = Vector.subtract(chain.get(n - 2 - i).position(), chain.get(n - 1 - i).position());
+            Quaternion q = new Quaternion(reversed.get(i).displacement(v1), reversed.get(i).displacement(v2));
+            reversed.get(i).rotate(q);
+        }
+    }
+
+
     @Override
     protected void _update() {
-
+        //if(st == 0) {
+            //_update(_original, _reversed);
+            //_updateReverse(_original, _reversed);
+        //}
     }
 
     @Override
     protected boolean _changed() {
-        return false;
+        if (_target == null) {
+            _prevTarget = null;
+            return false;
+        } else if (_prevTarget == null) {
+            return true;
+        }
+        return !(_prevTarget.position().matches(_target.position()) && _prevTarget.orientation().matches(_target.orientation()));
     }
 
     @Override
     protected void _reset() {
-
+        _prevTarget = _target == null ? null : new Node(_target.position().get(), _target.orientation().get(), 1);
+        _iterations = 0;
+        st = 0;
+        _updateReverse(_original, _reversed);
+        translateChain(_reversed, _target.position());
     }
 
     @Override
     public float error() {
-        return 0;
+        return Vector.distance(_chain.get(_chain.size() - 1).position(), _target.position());
     }
 
-    @Override
     public void setTarget(Node endEffector, Node target) {
+        this._target = target;
+    }
 
+    public void setTarget(Node target) {
+        this._target = target;
     }
 
     //Step 1: Translate the whole chain to reach Target
@@ -74,79 +132,129 @@ public class MySolver extends Solver{
     //Step 2:
     //Goal: Decrease distance between j_0 and j_0_hat
     //Step 2.1 Stretch / Contract chain in order to to min | distance(j_i_hat, j_0_hat) - distance(j_i_hat, j_0) |
-    protected void applyLocalRotation(List<? extends Node> current, List<? extends Node> reversed, int i){
+    //Step 2.2 Apply a rotation that approach root and projected root as much as possible
+    protected void _applyStep(List<? extends Node> current, List<? extends Node> reversed, int i){
+        System.out.println("=========================");
+        System.out.println("=========================");
+        System.out.println("i :" + i);
         int i_cur = current.size() - 1 - i;
         //Get how much to extend
-        Vector j_i_j_0 = Vector.subtract(current.get(i_cur).position(), current.get(0).position());
-        Vector j_i_hat_j_0 = Vector.subtract(reversed.get(i).position(), current.get(0).position());
-        float desired_extension = j_i_j_0.magnitude();
-        float current_extension = j_i_hat_j_0.magnitude();
+        Vector root_j_i_hat = Vector.subtract(reversed.get(i).position(), current.get(0).position());
+        Vector root_hat_j_i_hat = Vector.subtract(reversed.get(i).position(), reversed.get(reversed.size() - 1).position());
+        float desired_extension = root_j_i_hat.magnitude();
+        float current_extension = root_hat_j_i_hat.magnitude();
         float difference = desired_extension - current_extension;
         //distribute the job between remaining Joints and current one
         //TODO: use constraints
-        float remaining_posible_extension = 0;
-        float local_posible_extension = 0;
-        boolean extend = true;
+        float remaining_possible_extension = 0;
+        float local_possible_extension = 0;
+        Vector dir = Vector.subtract(reversed.get(i + 1).position(), reversed.get(i).position());
         if(difference > 0){
-            remaining_posible_extension = _posibleExtension(reversed, i + 1, reversed.size() - 1);
-            local_posible_extension = _posibleExtension(reversed, i-1, i);
+            remaining_possible_extension = _possibleGlobalExtension(reversed, i + 2, reversed.size() - 1);
+            local_possible_extension = _possibleLocalExtension(reversed, i);
+            System.out.println("Ext : " + remaining_possible_extension + " Local : " + local_possible_extension);
         }else{
-            extend = false;
-            remaining_posible_extension = _posibleContraction(reversed, i + 1, reversed.size() - 1);
-            local_posible_extension = _posibleContraction(reversed, i-1, i);
+            //TODO: Check this with pre-calculations
+            remaining_possible_extension = _possibleGlobalContraction(reversed, i + 2, reversed.size() - 1);
+            local_possible_extension = _possibleLocalContraction(reversed, i);
+            System.out.println("Cont : " + remaining_possible_extension + " Local : " + local_possible_extension);
         }
-        float extension = local_posible_extension;
-        extension *= _localExtension(remaining_posible_extension, local_posible_extension, desired_extension, i);
+        float extension = Math.signum(difference) * _localExtension(remaining_possible_extension, local_possible_extension, Math.abs(difference), Math.max(reversed.size() - i - 2, 1));
+        System.out.println("Current ext : " + current_extension);
+        System.out.println("Desired ext : " + desired_extension);
+        System.out.println("Extension final : " + extension);
         //Apply defined extension
+        if(cur == 0) {
+            _applyExtension(reversed, i + 1, extension);
+            cur++;
+        }
+        else if(cur == 1){
+            _applyRotation(reversed,i, current.get(0).position());
+            cur = 0;
+            st++;
+        }
+    }
 
 
-
+    protected float _possibleLocalExtension(List<? extends Node> chain, int i){
+        return _possibleGlobalExtension(chain, i, i + 2);
     }
 
     //TODO: Do the same calculation but with constrained structures
-    protected float _posibleExtension(List<? extends Node> chain, int init, int end){
+    //How much could the chain extend given its current configuration
+    protected float _possibleGlobalExtension(List<? extends Node> chain, int init, int end){
+        if(init >= chain.size() || init >= end || end >= chain.size()) return 0;
+        Vector current = Vector.subtract(chain.get(init).position(), chain.get(end).position());
+        //Vector proj = Vector.projectVectorOnAxis(current, axis);
+        float current_extension = current.magnitude();
         float max = 0;
-        for(int i = init + 1; i <= end; i++){
-            Vector bone = Vector.subtract(chain.get(i).reference().position(), chain.get(i).position());
+        for(int i = init; i < end; i++){
+            Vector bone = Vector.subtract(chain.get(i).position(), chain.get(i + 1).position());
             max += bone.magnitude();
         }
-        return max;
+        System.out.println("°°°°°°°°°° MAX " + max);
+        System.out.println("°°°°°°°°°° Curr " + current);
+        System.out.println("°°°°°°°°°° Curr_ext " + current_extension);
+
+        return max - current_extension;
+    }
+
+    protected float _possibleGlobalContraction(List<? extends Node> chain, int init, int end){
+        if(init >= chain.size() || init >= end || end >= chain.size()) return 0;
+        return Vector.subtract(chain.get(init).position(), chain.get(end).position()).magnitude();
     }
 
     //TODO: Do the same calculation but with constrained structures
-    protected float _posibleContraction(List<? extends Node> chain, int init, int end){
-        return Vector.subtract(chain.get(init).position(), chain.get(end).position()).magnitude();
+    protected float _possibleLocalContraction(List<? extends Node> chain, int i){
+        return _possibleGlobalContraction(chain, i, i + 2);
     }
 
     //define how to extend/contract the local joint. the idea is to try to make all joint to move the same amount
     protected float _localExtension(float remaining, float local, float desired, int n){
-        float avg_extension = (remaining + local)/n;
+        float avg_extension = desired/n;
+        System.out.println("Avg : " + avg_extension);
+        System.out.println("local : " + local);
         if(remaining + local < desired || remaining + avg_extension < desired || avg_extension > local){
-            return 1;
+            return local;
         }
-        return Math.max(avg_extension,Math.min(local, avg_extension * 1.1f)) / local;
+        return Math.max(avg_extension,Math.min(local, avg_extension * 1.1f));
     }
 
     protected void _applyExtension(List<? extends Node> reversed, int i, float extension){
+        if(i >= reversed.size() - 1) return;
+        Node j_i_prev = reversed.get(i - 1);
         Node j_i = reversed.get(i);
-        Node j_i1 = reversed.get(i + 1);
-        //Do all w.r.t h_i1
-        Vector dir = j_i1.location(j_i).get();
-        dir.multiply(extension > 0 ? -1 : 1);
-        Vector j_root = j_i1.location(reversed.get(0));
-        float desired_theta = (float) Math.acos(extension / j_root.magnitude());
-        float current_theta = Vector.angleBetween(j_root, dir);
-        Vector axis = Vector.cross(j_root, dir, null);
-        if(axis.magnitude() < 1e-5) j_root.orthogonalVector();
+        Node j_i_next = reversed.get(i + 1);
+        //Law of Cosine
+        float a = Vector.subtract(j_i_prev.position(), j_i.position()).magnitude();
+        float b = Vector.subtract(j_i_next.position(), j_i.position()).magnitude();
+        float current_extension = Vector.subtract(j_i_prev.position(), j_i_next.position()).magnitude();
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+        System.out.println("---| cur extension : " + current_extension);
+        System.out.println("---| extension : " + extension);
+
+        float c = Math.max(Math.min(Math.abs(current_extension + extension), Math.abs(a + b)), Math.abs(a - b));
+        float desired_theta = (float) Math.acos((-c*c + a*a + b*b)/(2*a*b));
+        //Do all w.r.t j_i1
+        System.out.println("---| desired_theta : " + Math.toDegrees(desired_theta));
+        float current_theta = Vector.angleBetween(j_i.location(j_i_prev), j_i.location(j_i_next));
+        System.out.println("---| current_theta : " + Math.toDegrees(current_theta));
+        Vector axis = Vector.cross(j_i.location(j_i_prev), j_i.location(j_i_next), null);
+        if(axis.magnitude() < 1e-5) axis = j_i_next.translation().orthogonalVector();
         float delta = desired_theta - current_theta;
-        j_i1.rotate(axis, delta);
+        System.out.println("---| delta : " + Math.toDegrees(delta));
+        System.out.println("---| axis : " + axis);
+        j_i.rotate(axis, delta);
+        System.out.println("---| final extension : " + Vector.subtract(j_i_prev.position(), j_i_next.position()).magnitude());
         return;
     }
 
     protected void _applyRotation(List<? extends Node> reversed, int i, Vector root){
         //apply a rotation that approach chain to original root
         Node j_i = reversed.get(i);
-        Vector root_hat = j_i.location(reversed.get(0));
+        root = j_i.location(root);
+        Vector root_hat = j_i.location(reversed.get(reversed.size() - 1));
         Quaternion desired_rotation = new Quaternion(root_hat, root);
         j_i.rotate(desired_rotation);
     }
@@ -172,6 +280,11 @@ public class MySolver extends Solver{
 
 
 
+
+    //TODO: Remove this!
+    public List<Node> reversed(){
+        return _reversed;
+    }
 
     protected void _generateGlobalConstraints(){
         _globalConstraints = new BallAndSocket[_chain.size()];
