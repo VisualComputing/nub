419d9b1e17c101976aa2d34a59d5ced93881d515
Sebastian Chaparro <sebchaparr@gmail.com>
2019-09-20 14:16:43 -0500

    CCD is now working with KinematicStructure and SolverAnimation

--- src/nub/ik/solver/geometric/CCDSolver.java
+++ src/nub/ik/solver/geometric/CCDSolver.java
@@ -13,9 +13,12 @@ package nub.ik.solver.geometric;
 
 import nub.core.Node;
 import nub.ik.animation.IKAnimation;
+import nub.ik.animation.SolverAnimation;
+import nub.ik.solver.KinematicStructure;
 import nub.ik.solver.Solver;
 import nub.primitives.Quaternion;
 import nub.primitives.Vector;
+import nub.processing.Scene;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +27,19 @@ import java.util.List;
 
 public class CCDSolver extends Solver {
   protected List<? extends Node> _chain;
+  //TODO: This is just for debug purposes
+  public boolean _enable_kinematic_structure = true;
+  //Using a Kinematic Structure decreases the number of transformations
+  protected List<KinematicStructure.KNode> _structure;
   protected Node _target;
   protected Node _previousTarget;
 
   //Animation Stuff
   protected boolean _enableHistory;
   protected IKAnimation.NodeStates _history;
+
+  //TODO: Refactor, perhaps move to Solver class
+  protected SolverAnimation _solverAnimation;
 
   public void enableHistory(boolean enableHistory) {
     _enableHistory = enableHistory;
@@ -63,9 +73,19 @@ public class CCDSolver extends Solver {
     this(chain, null);
   }
 
+  public CCDSolver(List<? extends Node> chain, boolean enable_kinematic_structure) {
+        this(chain, null, enable_kinematic_structure);
+    }
+
   public CCDSolver(List<? extends Node> chain, Node target) {
+    this(chain, target, true);
+  }
+
+  public CCDSolver(List<? extends Node> chain, Node target, boolean enable_kinematic_structure) {
     super();
-    this._chain = chain;
+    _enable_kinematic_structure = enable_kinematic_structure;
+    if(_enable_kinematic_structure) this._structure = KinematicStructure.generateKChain(chain);
+    else this._chain = chain;
     this._target = target;
     this._previousTarget =
         target == null ? null : new Node(target.position().get(), target.orientation().get(), 1);
@@ -78,6 +98,12 @@ public class CCDSolver extends Solver {
    * */
   @Override
   protected boolean _iterate() {
+    if(_enable_kinematic_structure) return iterateWithStructure();
+    return iterateWithChain();
+  }
+
+  //TODO: this must be removed
+  protected boolean iterateWithChain(){
     //As no target is specified there is no need to perform an iteration
     if (_target == null || _chain.size() < 2) return true;
     Node end = _chain.get(_chain.size() - 1);
@@ -89,8 +115,6 @@ public class CCDSolver extends Solver {
     float change = 0.0f;
     Vector endLocalPosition = _chain.get(_chain.size() - 2).location(end.position());
     Vector targetLocalPosition = _chain.get(_chain.size() - 2).location(target);
-
-    ArrayList<Vector> positions = new ArrayList<Vector>();
 
     for (int i = _chain.size() - 2; i >= 0; i--) {
       Quaternion initial = _chain.get(i).rotation().get();
@@ -108,21 +132,67 @@ public class CCDSolver extends Solver {
       } else {
         endLocalPosition = _chain.get(i).reference().location(endLocalPosition, _chain.get(i));
       }
-      initial.compose(_chain.get(i).rotation().get());
+      initial.compose(_chain.get(i).rotation().inverse());
       change += Math.abs(initial.angle());
-      positions.add(0, _chain.get(i + 1).position().get());
-
       if (_enableHistory) {
         history().addNodeState("step", _chain.get(i), _chain.get(i).reference(), null, _chain.get(i).rotation().get());
         history().incrementStep();
       }
     }
-    positions.add(0, _chain.get(0).position().get());
     if (_enableHistory) history().incrementIteration();
     //Check total rotation change
     if (change <= _minDistance) return true;
     return false;
   }
+
+  protected boolean iterateWithStructure(){
+    //As no target is specified there is no need to perform an iteration
+    if (_target == null || _structure.size() < 2) return true;
+    KinematicStructure.KNode end = _structure.get(_structure.size() - 1);
+    Vector target = this._target.position().get();
+    //update whole chain position/orientation
+    Vector endPosition = end.position().get();
+    //Execute Until the distance between the end effector and the target is below a threshold
+    if (Vector.distance(end.position(), target) <= _maxError) {
+      return true;
+    }
+
+    float change = 0.0f;
+
+    if(_solverAnimation != null) _solverAnimation.enableSequential(true);
+    //Traverse the structure backwards
+    for (int i = _structure.size() - 2; i >= 0; i--) {
+      Vector targetLocalPosition = _structure.get(i).location(target, true);
+      Vector endLocalPosition = _structure.get(i).location(end.position(), true);
+      if(_solverAnimation != null) {
+        _solverAnimation.enableSequential(true);
+        _solverAnimation.addTrajectory(null, _structure.get(i).position(), end.position());
+        _solverAnimation.addTrajectory(null, _structure.get(i).position(), target);
+        _solverAnimation.enableSequential(false);
+        _solverAnimation.addTrajectory(_structure.get(i).node(), new Vector(), endLocalPosition);
+      }
+
+      Quaternion delta = new Quaternion(endLocalPosition, targetLocalPosition);
+      _structure.get(i).rotate(delta);
+
+      if(_solverAnimation != null){
+        _solverAnimation.addRotateNode(_structure.get(i).node(), delta);
+        _solverAnimation.enableSequential(true);
+        _solverAnimation.clearStep(4);
+      }
+
+      //update end effector local position
+      if (_enableHistory) {
+        history().addNodeState("step", _structure.get(i).node(), _structure.get(i).node().reference(), null, _structure.get(i).rotation().get());
+        history().incrementStep();
+      }
+    }
+    if (_enableHistory) history().incrementIteration();
+    //Check total change
+    if (Vector.distance(endPosition, end.position()) <= _minDistance) return true;
+    return false;
+  }
+
 
   @Override
   protected void _update() {
@@ -148,12 +218,19 @@ public class CCDSolver extends Solver {
       if (_history == null) _history = new IKAnimation.NodeStates();
       else _history.clear();
     }
-    ArrayList<Vector> positions = new ArrayList<Vector>();
-    for (Node node : chain()) {
-      Vector position = node.position().get();
-      positions.add(position);
-      if (_enableHistory)
-        _history.addNodeState("initialization", node, node.reference(), node.translation().get(), node.rotation().get());
+    if(_enable_kinematic_structure){
+        //TODO: Remove this update!
+        _structure.get(_structure.size()-1).updatePath(null);
+      for (KinematicStructure.KNode node : _structure) {
+        if (_enableHistory)
+          _history.addNodeState("initialization", node.node(), node.node().reference(), node.translation().get(), node.rotation().get());
+      }
+    }
+    else {
+      for (Node node : chain()) {
+        if (_enableHistory)
+          _history.addNodeState("initialization", node, node.reference(), node.translation().get(), node.rotation().get());
+      }
     }
     if (_enableHistory) {
       _history.incrementStep();
@@ -163,6 +240,7 @@ public class CCDSolver extends Solver {
 
   @Override
   public float error() {
+    if(_enable_kinematic_structure) return Vector.distance(_structure.get(_structure.size() - 1).position(), _target.position());
     return Vector.distance(_chain.get(_chain.size() - 1).position(), _target.position());
   }
 
@@ -171,4 +249,21 @@ public class CCDSolver extends Solver {
     return _history;
   }
 
+  //TODO: Refactor, perhaps move to Solver class
+  public void attachSolverAnimation(SolverAnimation solverAnimation){
+      _solverAnimation = solverAnimation;
+      registerStructures();
+  }
+
+  public void registerStructures(){
+      if(_enable_kinematic_structure){
+          List<Node> chain = new ArrayList<Node>();
+          for(KinematicStructure.KNode knode : _structure){
+              chain.add(knode.node());
+          }
+          _solverAnimation.registerStructure("Chain", chain);
+      }else {
+          _solverAnimation.registerStructure("Chain", _chain);
+      }
+  }
 }
