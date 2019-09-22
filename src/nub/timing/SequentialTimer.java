ccdd0accfd48575910eb8b5affa2f2180d3c4e1a
Jean Pierre Charalambos <nakednous@gmail.com>
2019-09-22 10:41:03 -0500

    Copyright notice updated

--- src/nub/timing/SequentialTimer.java
+++ src/nub/timing/SequentialTimer.java
@@ -1,12 +1,12 @@
-/****************************************************************************************
+/******************************************************************************************
  * nub
- * Copyright (c) 2019 National University of Colombia, https://visualcomputing.github.io/
+ * Copyright (c) 2019 Universidad Nacional de Colombia, https://visualcomputing.github.io/
  * @author Jean Pierre Charalambos, https://github.com/VisualComputing
  *
  * All rights reserved. A 2D or 3D scene graph library providing eye, input and timing
  * handling to a third party (real or non-real time) renderer. Released under the terms
  * of the GPL v3.0 which is available at http://www.gnu.org/licenses/gpl.html
- ****************************************************************************************/
+ ******************************************************************************************/
 
 package nub.timing;
 
