16777b448072af3b4a10a3fa1e2788dde38f62d7
Sebastian Chaparro <sebchaparr@gmail.com>
2019-10-06 00:12:33 -0500

    EventToViz has been updated

--- src/nub/ik/animation/EventToViz.java
+++ src/nub/ik/animation/EventToViz.java
@@ -1,9 +1,12 @@
 package nub.ik.animation;
 
+import nub.core.Node;
 import nub.primitives.Quaternion;
 import nub.primitives.Vector;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * This class includes common implementations to map an Interesting Event into a Visual Step
@@ -31,6 +34,9 @@ public class EventToViz {
             case "NodeRotation":{
                 return generateRotateNode(visualizer, event);
             }
+            case "UpdateStructure":{
+                return generateUpdateStructure(visualizer, event);
+            }
         }
         return null;
     }
@@ -42,6 +48,8 @@ public class EventToViz {
     public static VisualSteps.RotateNode generateRotateNode(Visualizer visualizer, float radius, InterestingEvent event){
         long executionDuration = event.executionDuration() * visualizer._stepStamp;
         long renderingDuration = event.renderingDuration() * visualizer._stepStamp;
+        System.out.println(visualizer._nodeToJoint.size());
+        System.out.println(visualizer._nodeToJoint.get(event.getAttribute("node")));
         VisualSteps.RotateNode  visualStep = new VisualSteps.RotateNode(visualizer._scene, visualizer._nodeToJoint.get(event.getAttribute("node")), radius, visualizer._period, executionDuration, renderingDuration);
         visualStep.setRotation((Quaternion) event.getAttribute("rotation"));
         return visualStep;
@@ -68,6 +76,19 @@ public class EventToViz {
         VisualSteps.FollowTrajectoryStep visualStep = new VisualSteps.FollowTrajectoryStep(visualizer._scene, radius, visualizer._period, executionDuration, renderingDuration);
         Vector[] positions = Arrays.asList((Object[]) event.getAttribute("positions")).toArray(new Vector[((Object[])event.getAttribute("positions")).length]);
         visualStep.setTrajectory(visualizer._nodeToJoint.get(event.getAttribute("reference")), positions);
+        return visualStep;
+    }
+
+    public static VisualSteps.UpdateStructure generateUpdateStructure(Visualizer visualizer, InterestingEvent event) {
+        long executionDuration = event.executionDuration() * visualizer._stepStamp;
+        long renderingDuration = event.renderingDuration() * visualizer._stepStamp;
+        List<Node> structure = new ArrayList<Node>();
+        for(Node node : (List<? extends Node>) event.getAttribute("structure")){
+            structure.add(visualizer._nodeToJoint.get(node));
+        }
+        VisualSteps.UpdateStructure  visualStep = new VisualSteps.UpdateStructure(visualizer._scene, structure, visualizer._period, executionDuration, renderingDuration);
+        visualStep.setFinalRotations((Quaternion[]) event.getAttribute("rotations"));
+        visualStep.setFinalTranslations((Vector[]) event.getAttribute("translations"));
         return visualStep;
     }
 
