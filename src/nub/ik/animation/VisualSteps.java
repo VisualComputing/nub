9d6a3714536c7ac30deb0a6653e302ae15b1bd75
Sebastian Chaparro <sebchaparr@gmail.com>
2019-10-06 00:11:39 -0500

    Update step added

--- src/nub/ik/animation/VisualSteps.java
+++ src/nub/ik/animation/VisualSteps.java
@@ -8,6 +8,8 @@ import nub.processing.Scene;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
+import java.util.List;
+
 /**
  * Here you could find some common Visual Steps used in visualization of IK algorithms
  * */
@@ -73,7 +75,7 @@ public class VisualSteps {
 
         @Override
         public void render() {
-            if(_mode == RotateNode.Rotate.NONE) return;
+            if(_mode == Rotate.NONE) return;
             PGraphics pg = _scene.context();
             pg.pushStyle();
             pg.hint(PConstants.DISABLE_DEPTH_TEST);
@@ -310,4 +312,98 @@ public class VisualSteps {
         pg.ellipse(v2.x(), v2.y(), radius, radius);
         pg.popStyle();
     }
+
+    public static class UpdateStructure extends VisualStep {
+        protected List<? extends Node> _structure;
+        protected Vector[] _initialTranslations, _finalTranslations, _deltaPerFrameTranslations, _deltaTranslations;
+        protected Quaternion[] _initialRotations, _finalRotations, _deltaPerFrameRotations, _deltaRotations;
+
+
+        public UpdateStructure(Scene scene, List<? extends Node> structure, long period, long duration, long renderingDuration) {
+            super(scene, period, duration, renderingDuration);
+            _structure = structure;
+        }
+
+        public void setFinalState(Vector[] translations, Quaternion[] rotations){
+            _finalTranslations = translations;
+            _finalRotations = rotations;
+        }
+
+        public void setFinalTranslations(Vector[] translations){
+            _finalTranslations = translations;
+        }
+
+        public void setFinalRotations(Quaternion[] rotations){
+            _finalRotations = rotations;
+        }
+
+        protected void _calculateSpeedPerIteration() {
+            float inc = ((float) _period) / (_duration - _duration % _period);
+            //Calculate deltas per frame
+            for(int i = 0; i < _structure.size(); i++){
+                _deltaPerFrameRotations[i] = new Quaternion(_deltaRotations[i].axis(), _deltaRotations[i].angle() * inc);
+                _deltaPerFrameTranslations[i] = Vector.multiply(_deltaTranslations[i], inc);
+            }
+        }
+
+
+        @Override
+        public void initialize() {
+            int n =  _structure.size();
+            _initialTranslations = new Vector[n];
+            _deltaTranslations = new Vector[n];
+            _deltaPerFrameTranslations = new Vector[n];
+            _initialRotations = new Quaternion[n];
+            _deltaRotations = new Quaternion[n];
+            _deltaPerFrameRotations = new Quaternion[n];
+            for(int i = 0; i < n; i++){
+                Node node =  _structure.get(i);
+                _initialTranslations[i] = node.translation().get();
+                _initialRotations[i] = node.rotation().get();
+                _deltaRotations[i] = Quaternion.compose(node.rotation().inverse(), _finalRotations[i]);
+                _deltaTranslations[i] = Vector.subtract(_finalTranslations[i], node.translation());
+            }
+            _completed = false;
+            _times = 0;
+            _calculateSpeedPerIteration();
+            //how many times to execute?
+            _totalTimes = (int) Math.ceil( 1.0* _duration / _period);
+            //how many times to render?
+            _totalRenderingTimes = (int) Math.ceil( 1.0* _renderingDuration / _period);
+        }
+
+        @Override
+        public void reverse() {
+
+        }
+
+        @Override
+        public void execute() {
+            if(_completed && _times >= _totalRenderingTimes){
+                _keepDrawing = false;
+            } else if (_times >= _totalTimes) {
+                //set rotation to fit exactly final rotation
+                for(int i = 0; i < _structure.size(); i++){
+                    _structure.get(i).setRotation(_finalRotations[i]);
+                    _structure.get(i).setTranslation(_finalTranslations[i]);
+                }
+                _completed = true;
+            }else{
+                for(int i = 0; i < _structure.size(); i++) {
+                    if (1 - Math.pow(Quaternion.dot(_structure.get(i).rotation(), _finalRotations[i]), 2) >= 0.000001) {
+                        _structure.get(i).rotate(_deltaPerFrameRotations[i]);
+                        _structure.get(i).translate(_deltaPerFrameTranslations[i]);
+                    }
+                }
+            }
+            _times++;
+        }
+
+        @Override
+        public void render() {
+            //Do nothing
+        }
+    }
+
+
 }
