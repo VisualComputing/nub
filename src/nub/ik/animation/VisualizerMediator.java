10ea96f479913836d555737c8c56ba043016ca8b
Sebastian Chaparro <sebchaparr@gmail.com>
2019-10-18 22:45:00 -0500

    Event adding methods updated.

--- src/nub/ik/animation/VisualizerMediator.java
+++ src/nub/ik/animation/VisualizerMediator.java
@@ -20,6 +20,7 @@ public class VisualizerMediator {
     protected List<Visualizer> _visualizers = new ArrayList<>();
     protected Solver _solver;
     protected List<InterestingEvent> _eventQueue = new ArrayList<>();
+
     protected long _firstEvent = 0;
     protected final long MAX_EVENT_SIZE = 5000;
 
@@ -30,6 +31,10 @@ public class VisualizerMediator {
         for(Visualizer v : visualizers){
             addVisualizer(v);
         }
+    }
+
+    public int finishingTime(){
+        return !_eventQueue.isEmpty() ? _eventQueue.get(_eventQueue.size() - 1).finishingTime() : 0;
     }
 
     public long lastEvent(){
@@ -52,23 +57,112 @@ public class VisualizerMediator {
         v._time = (_firstEvent + _eventQueue.size()) * v._period;
     }
 
-
+    //Methods for facilitating event insertion on events queue
     public void addEvent(InterestingEvent event){
+        System.out.print("name " + event.name());
+        System.out.print(" start " + event.startingTime());
+        System.out.println(" duration " + event.executionDuration());
+
         //Insert the event at its corresponding position o(n)
         if(_eventQueue.isEmpty()){
             _eventQueue.add(event);
             return;
         }
+
+        boolean added = false;
         for(int i = _eventQueue.size() -1; i >= 0; i--){
-            if(event.startingTime() >= _eventQueue.get(i).startingTime()){
+            InterestingEvent current = _eventQueue.get(i);
+            if(event.startingTime() > current.startingTime() ||  (event.startingTime() == current.startingTime() && event.finishingTime() >= current.finishingTime())){
                 if(i + 1 < _eventQueue.size()) _eventQueue.add(i + 1, event);
                 else _eventQueue.add(event);
+                added = true;
                 break;
             }
         }
+        if(!added) _eventQueue.add(0,event);
         if(_eventQueue.size() > MAX_EVENT_SIZE){
             _limitEventQueue();
         }
+    }
+
+    public InterestingEvent lastEventWithName(String name){
+        for(int i = _eventQueue.size() - 1; i >= 0; i--){
+            if(_eventQueue.get(i).name().equals(name)) return _eventQueue.get(i);
+        }
+        return null;
+    }
+
+    public InterestingEvent addEvent(String name, String type, int startingTime, int executionDuration, int renderingDuration){
+        InterestingEvent event = new InterestingEvent(name, type);
+        event.startAt(startingTime);
+        event.setDuration(executionDuration, renderingDuration);
+        addEvent(event);
+        return event;
+    }
+
+    public InterestingEvent addEventStartingAfter(InterestingEvent previous, String name, String type, int executionDuration, int renderingDuration, int wait){
+        int start = previous.finishingTime() + wait;
+        return  addEvent(name, type, start, executionDuration, renderingDuration);
+    }
+
+    public InterestingEvent addEventStartingAfter(InterestingEvent previous, String name, String type, int executionDuration, int renderingDuration){
+        return  addEventStartingAfter(previous, name, type, executionDuration, renderingDuration, 0);
+    }
+
+    public InterestingEvent addEventStartingAfter(InterestingEvent previous, String name, String type, int executionDuration){
+        return  addEventStartingAfter(previous, name, type, executionDuration, executionDuration, 0);
+    }
+
+    //Starts the event right after the last event with the given name (if there's no one an error will be thrown)
+    public InterestingEvent addEventStartingAfter(String previousEventName, String name, String type, int executionDuration, int renderingDuration, int wait){
+        InterestingEvent event = lastEventWithName(previousEventName);
+        if(event == null) throw new RuntimeException("There is no event with name " + previousEventName);
+        return  addEventStartingAfter(event, name, type, executionDuration, renderingDuration, wait);
+    }
+
+    public InterestingEvent addEventStartingAfter(String previousEventName, String name, String type, int executionDuration, int renderingDuration){
+        return  addEventStartingAfter(previousEventName, name, type, executionDuration, renderingDuration, 0);
+    }
+
+    public InterestingEvent addEventStartingAfter(String previousEventName, String name, String type, int executionDuration){
+        return  addEventStartingAfter(previousEventName, name, type, executionDuration, executionDuration, 0);
+    }
+
+    public InterestingEvent addEventStartingAfterLast(String name, String type, int executionDuration, int renderingDuration, int wait){
+        return  addEventStartingAfter(_eventQueue.get(_eventQueue.size() - 1), name, type, executionDuration, renderingDuration, wait);
+    }
+
+    public InterestingEvent addEventStartingAfterLast(String name, String type, int executionDuration, int renderingDuration){
+        System.out.println("finishing time : " + finishingTime());
+        return  addEvent(name, type, finishingTime(),executionDuration, renderingDuration);
+    }
+
+    public InterestingEvent addEventStartingAfterLast(String name, String type, int executionDuration){
+        return  addEvent(name, type, finishingTime(),executionDuration, executionDuration);
+    }
+
+    public InterestingEvent addEventStartingWith(InterestingEvent event, String name, String type, int executionDuration, int renderingDuration){
+        int start = event.startingTime();
+        return  addEvent(name, type, start, executionDuration, renderingDuration);
+    }
+
+    public InterestingEvent addEventStartingWith(String concurrentEventName, String name, String type, int executionDuration, int renderingDuration){
+        InterestingEvent event = lastEventWithName(concurrentEventName);
+        if(event == null) throw new RuntimeException("There is no event with name " + concurrentEventName);
+        return  addEventStartingWith(event, name, type, executionDuration, renderingDuration);
+    }
+
+    public InterestingEvent addEventStartingWith(String concurrentEventName, String name, String type, int executionDuration){
+        return  addEventStartingWith(concurrentEventName, name, type, executionDuration, executionDuration);
+    }
+
+    public InterestingEvent addEventStartingWithLast(String name, String type, int executionDuration, int renderingDuration){
+        if(_eventQueue.isEmpty()) return addEvent(name, type, finishingTime(), executionDuration, renderingDuration);
+        return  addEventStartingWith(_eventQueue.get(_eventQueue.size() - 1), name, type, executionDuration, renderingDuration);
+    }
+
+    public InterestingEvent addEventStartingWithLast(String name, String type, int executionDuration){
+        return  addEventStartingWithLast(name, type, executionDuration, executionDuration);
     }
 
     protected void _limitEventQueue(){
