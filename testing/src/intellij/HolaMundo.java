9d308baf6e3faaf1af49df56a0228b60b265650b
nakednous <nakednous@gmail.com>
2020-05-31 14:37:33 -0500

    example improved

--- testing/src/intellij/HolaMundo.java
+++ testing/src/intellij/HolaMundo.java
@@ -5,34 +5,12 @@ import nub.primitives.Vector;
 import nub.processing.Scene;
 import processing.core.PApplet;
 import processing.core.PGraphics;
+import processing.core.PImage;
 import processing.core.PShape;
 import processing.event.MouseEvent;
 import processing.opengl.PShader;
 
 public class HolaMundo extends PApplet {
-  /*
-  // 1. Nodes
-  Node root, torus, box, can;
-  // 2. Main-Scene
-  String renderer = P3D;
-  Scene mainScene;
-  // 3. Visual hint off-screen scene
-  Scene hintScene;
-  int w, h;
-  int atX, atY;
-  // 4. can off-screen scene
-  Scene canScene;
-  boolean displayCan;
-  PShader texShader;
-  // 5. Scene handler
-  Scene scene;
-  // 6. Debug (draw) ray
-  Vector orig = new Vector();
-  Vector dir = new Vector();
-  Vector end = new Vector();
-  Vector pup;
-   */
-
   // 1. Nodes
   Node root, torus, box;
   // 2. Main-Scene
@@ -60,15 +38,27 @@ public class HolaMundo extends PApplet {
 
   @Override
   public void setup() {
+    // A. Scenes
+    // 1. Main (onscreen) Scene
     mainScene = new Scene(this);
     mainScene.setRadius(500);
     mainScene.fit(1);
     mainScene.setVisualHint(Scene.AXES | Scene.GRID);
     mainScene.configHint(Scene.GRID, color(0, 255, 0));
+    // 2. Hint (offscreen) Scene
     hintScene = new Scene(this, P3D, w, h);
     hintScene.setRadius(300);
+    /*
+    // 3. Can (offscreen) Scene
+    canScene = new Scene(this, P3D, 500, 500);
+    canScene.setRadius(500);
+    canScene.fit();
+     */
+    // B. Nodes
+    // 1. root (mainScene and hintScene only)
     root = new Node();
     root.disableTagging();
+    // 2. torus
     torus = new Node(root, (pg) -> {
       pg.push();
       pg.fill(255, 0, 0);
@@ -78,28 +68,48 @@ public class HolaMundo extends PApplet {
     torus.scale(10);
     torus.translate(-200, -200, 0);
     torus.setPickingThreshold(0);
+    // 3. box
     PShape pbox = createShape(BOX, 200);
     pbox.setFill(color(255, 255, 0));
     box = new Node(root, pbox);
     box.translate(200, 200, 0);
     box.setPickingThreshold(0);
+    // 4. can (canScene only)
+    //can = new Node(createCan(100, 200, 32, scene.context()));
+    //can = new Node(createCan(100, 200, 32, loadImage("/home/pierre/IdeaProjects/nub/testing/data/texture/lachoy.jpg")));
   }
 
   @Override
   public void draw() {
-    scene = pup != null ? hintScene.hasMouseFocus() ? hintScene : mainScene : mainScene;
+    scene = hintScene.hasMouseFocus() ? hintScene : mainScene;
+    //scene = hintScene.hasMouseFocus() ? hintScene : canScene.hasMouseFocus() ? canScene : mainScene;
     background(0);
+    // subtree rendering
     mainScene.render(root);
     if (pup != null) {
+      // debug
       drawRay();
       mainScene.beginHUD();
       hintScene.beginDraw();
       hintScene.context().background(125, 80, 90);
+      // subtree rendering
       hintScene.render(root);
       hintScene.endDraw();
       hintScene.display(atX, atY);
       mainScene.endHUD();
     }
+    /*
+    if (displayCan) {
+      mainScene.beginHUD();
+      canScene.beginDraw();
+      canScene.context().background(125, 80, 90);
+      // subtree rendering
+      canScene.render(can);
+      canScene.endDraw();
+      canScene.display(atX, atY);
+      mainScene.endHUD();
+    }
+     */
   }
 
   @Override
@@ -147,9 +157,9 @@ public class HolaMundo extends PApplet {
       scene.toggleHint(Scene.GRID);
     } else if (key == 'a') {
       scene.toggleHint(Scene.AXES);
-    }/* else if (key == 'r') {
-      ray = !ray;
-    }*/
+    } else if (key == 'c') {
+      displayCan = !displayCan;
+    }
   }
 
   // debug ray
@@ -165,6 +175,25 @@ public class HolaMundo extends PApplet {
       pg.line(orig.x(), orig.y(), orig.z(), end.x(), end.y(), end.z());
       pg.popStyle();
     }
+  }
+
+  PShape createCan(float r, float h, int detail, PImage tex) {
+    textureMode(NORMAL);
+    PShape sh = createShape();
+    sh.beginShape(QUAD_STRIP);
+    sh.noStroke();
+    sh.texture(tex);
+    for (int i = 0; i <= detail; i++) {
+      float angle = TWO_PI / detail;
+      float x = sin(i * angle);
+      float z = cos(i * angle);
+      float u = (float) i / detail;
+      sh.normal(x, 0, z);
+      sh.vertex(x * r, -h / 2, z * r, u, 0);
+      sh.vertex(x * r, +h / 2, z * r, u, 1);
+    }
+    sh.endShape();
+    return sh;
   }
 
   public static void main(String[] args) {
